b48dcc32545fc34ce5c2d661ad7efdb8841d3fde
Twinkal <twinkal.work@outlook.com>
2022-11-07 20:06:22 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -37,37 +37,39 @@ function App() {
 
   return (
     <div className="App">
-      <div className="flex justify-center items-center p-10">
-        <div className="mt-8 flex flex-col">
-          <div className="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
-            <div className="inline-block mix-w-fit py-2 align-middle md:px-6 lg:px-8">
-              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                <table className="divide-gray-300">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                      >
-                        Wallet Address
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200 bg-white">
-                    {resultOfBayc.map((token) => (
+      {resultOfBayc.length > 0 && (
+        <div className="flex justify-center items-center p-10">
+          <div className="mt-8 flex flex-col">
+            <div className="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
+              <div className="inline-block mix-w-fit py-2 align-middle md:px-6 lg:px-8">
+                <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                  <table className="divide-gray-300">
+                    <thead className="bg-gray-50">
                       <tr>
-                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                          {token}
-                        </td>
+                        <th
+                          scope="col"
+                          className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                        >
+                          Wallet Address
+                        </th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
+                    </thead>
+                    <tbody className="divide-y divide-gray-200 bg-white">
+                      {resultOfBayc.map((token) => (
+                        <tr>
+                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                            {token}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
